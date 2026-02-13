4d680be41e9ae9eb3fb986452d8af114fecca6ce
Pallavibalkhande <97938109+Pallavibalkhande@users.noreply.github.com>
2026-02-13 20:04:32 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+      🚀 Simple React App
+    </h1>
+  );
+}
+
+export default App;
